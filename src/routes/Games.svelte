a4acbbec1870a963b85239ee3250f0ec562e64a1
Alex <alexnlarsen@gmail.com>
2021-04-18 11:46:02 +0200

    use Unity stylesheet and override dumb layouts

--- src/routes/Games.svelte
+++ src/routes/Games.svelte
@@ -3,5 +3,16 @@
   import UnityGame from "./Games/UnityGame.svelte";
 </script>
 
-<UnityGame game={"Roller"}/>
-<TicTacToe/>
+<div>
+  <h1>Roll A Ball</h1>
+  <UnityGame game={"Roller"} />
+  <h1>Tic Tac Toe</h1>
+  <TicTacToe />
+</div>
+
+<style>
+    div > * {
+        text-align: center;
+        margin-top: 10%;
+    }
+</style>
